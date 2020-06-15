969595feab934a45b2a30d9e21166d8b68c19f92
jakubjankowski99 <jakubj31@gmail.com>
2020-06-15 12:49:29 +0200

    Task 3&4

--- src/pl/wsb/human/Human.java
+++ src/pl/wsb/human/Human.java
@@ -3,6 +3,8 @@ package pl.wsb.human;
 import pl.wsb.animal.Animal;
 import pl.wsb.car.Car;
 
+
+
 public class Human {
 
     String firstName;
@@ -10,6 +12,8 @@ public class Human {
     double weight;
     Animal pet;
     Car vehicle;
+    private double salary;
+    private static String PASS = "mypass";
 
     public Human(String firstName, String lastName, double weight, Animal pet, Car vehicle) {
         this.firstName = firstName;
@@ -18,4 +22,22 @@ public class Human {
         this.pet = pet;
         this.vehicle = vehicle;
     }
+
+    public double getSalary(String pass) {
+        if (pass ==  PASS){
+            return salary;
+        }else{
+            return Double.parseDouble(null);
+        }
+    }
+
+    public void setSalary(Double salary, String pass) {
+        if (pass.equals(PASS) && salary > 0) {
+            this.salary = salary;
+            System.out.println("New salary: " + salary);
+            System.out.println("Remember to update your TAX info...");
+        } else
+            System.out.println("Salary has not been changed.");
+    }
+
 }
